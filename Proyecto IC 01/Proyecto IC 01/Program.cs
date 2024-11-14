c84b00f5c6f0a483a6ba9415fd32956c2a85bafa
Elmer Vásquez Guevara <vasquezguevaraelmer75@gmail.com>
2024-11-14 12:38:03 -0500

    Tercer cambio: He creado un formulario de ventas en c#

--- Proyecto IC 01/Proyecto IC 01/Program.cs
+++ Proyecto IC 01/Proyecto IC 01/Program.cs
@@ -1,4 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola mundo");
+
+
+string NombresApellidos = "Elmer Vasquez Guevara";
+int edad = 22;
+string sexo = "Masculino";
+string CarreraProfesional = "APSTI";
+string LugarEstudios = "IESTPCH";
+string UnidadDidactica = "Gestion de cambios de proyectios";
+
+
 Console.WriteLine("Mi nombre es Elmer");
-Console.WriteLine("Cree un archivo de c# ");
+Console.WriteLine("Cree un archivo llamado Integración continua");
+Console.WriteLine("Mi nombre es: {0}",NombresApellidos);
+Console.WriteLine("mi edad es {0}", edad);
+Console.WriteLine("Mi sexo es {0}",sexo);
+Console.WriteLine("Estudio la carrera profesional de: {0}",CarreraProfesional);
+Console.WriteLine("Se ubica en : {0}",LugarEstudios);
+Console.WriteLine("Este curso se llama : {0}",UnidadDidactica);
+
+
+
+
